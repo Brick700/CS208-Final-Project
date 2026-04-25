bad0185d7cfcb71c9ee1c21397bc949b3e06d071
Mason Beckman <masonbeckman@u.boisestate.edu>
2026-04-25 18:50:30 +0000

    Created emtpy field validation in case user input is empty

--- routes/index.js
+++ routes/index.js
@@ -29,61 +29,32 @@ router.get('/about', function(req, res, next){
 
 /** Comment Page */
 router.get('/comments', function(req, res, next){
+
+  //Pagination
+
+
   res.render('comments', { title: 'Downtown Donuts'});
 });
 
+router.post('/comments', function(req, res, next){
+const {name, comment } = req.body;
+
+//Reject Empty Fields
+if (!name || !name.trim() === '') {
+  return res.render('comments', {title: 'Donwtown Donuts', comments: [], error: 'A name is required',});
+}
+
+if (!comment || !comment.trim() === '') {
+  return res.render('comments', {title: 'Donwtown Donuts', comments: [], error: 'Comment cannot be blank',});
+}
 
 
+//Max character length
 
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
-});
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
-    try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
-        }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
-      });
-    } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
-    }
-});
+//Sanitize inputs
 
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
-    });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
-    }
-});
+//Timestamp
+
 
 module.exports = router;
